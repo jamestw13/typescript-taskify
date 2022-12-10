c01bdd37751af08cefd3f7b2ddd0020bc7867925
TJ James <jamestw13@gmail.com>
2022-12-10 14:19:13 -0600

    final touches

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -44,9 +44,9 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos, index }) => {
 
   return (
     <Draggable draggableId={todo.id.toString()} index={index}>
-      {provided => (
+      {(provided, snapshot) => (
         <form
-          className="todos__single"
+          className={`todos__single ${snapshot.isDragging && 'drag'}`}
           onSubmit={e => handleSubmit(e, todo.id)}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
